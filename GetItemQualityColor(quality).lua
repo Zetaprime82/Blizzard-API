2d9bf43a6ee13e3c579513a72f22b6428521fe11
Zetaprime82 <darklord_dm_@hotmail.com>
2022-10-07 14:01:20 +0200

    Update GetItemQualityColor(quality).lua

--- GetItemQualityColor(quality).lua
+++ GetItemQualityColor(quality).lua
@@ -1,1 +1,27 @@
+API_GetItemQualityColor
+--------------------
+r = 1 |g = 1 |b = 1
+--------------------
+0 Poor ff9d9d9d
+1 Common ffffffff
+2 Uncommon ff1eff00
+3 Rare ff0070dd
+4 Epic ffa335ee
+5 Legendary ffff8000
+6 Artifact ffe6cc80
+7 Heirloom ff00ccff
+8 WoW Token ff00ccff
+---------------
+hex |cffffffff
+---------------
+0 Poor |cff9d9d9d
+1 Common |cffffffff
+2 Uncommon |cff1eff00
+3 Rare |cff0070dd
+4 Epic |cffa335ee
+5 Legendary |cffff8000
+6 Artifact |cffe6cc80
+7 Heirloom |cff00ccff
+8 WoW Token |cff00ccff
 
+https://wowwiki-archive.fandom.com/wiki/API_GetItemQualityColor#toc
